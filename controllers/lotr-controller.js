f020659f88154bb2fa8699b13b39a740480f5c72
adama <adamatraoread86@gmail.com>
2024-04-18 11:03:42 -0600

    added new functions

--- controllers/lotr-controller.js
+++ controllers/lotr-controller.js
@@ -3,6 +3,14 @@ const axios = require("axios");
 const lotrURL= "https://the-one-api.dev/v2"
 const lotrKey = "gUHYDbXEQN1DURgsQYh1"
 
+
+const lotrAxios = axios.create({
+    baseUrl: lotrURL,
+    headers: {
+        Authorization: "Bearer "+ lotrKey
+    }
+})
+
 exports.getBooks = async (req, res, next) => {
     //Retrieve all books form lotr api
    const response = await axios.get("https://the-one-api.dev/v2/book");
@@ -15,5 +23,10 @@ exports.getMovies = async (req, res, next) => {
             Authorization: "Bearer "+ lotrKey
         }
     });
+    res.status(200).json(response.data);
+}
+
+exports.getCharacters = async (req, res, next) => {
+    const response = await lotrAxios.get("/characters")
     res.status(200).json(response.data);
 }
--- routes/lotr-routes.js
+++ routes/lotr-routes.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const lotrController = require('../controllers/lotr-controller');
 
-router.get("/books", lotrController.getBooks)
+router.get("/books", lotrController.getBooks);
 
-router.get("/movies", lotrController.getMovies)
+router.get("/movies", lotrController.getMovies);
+
+router.get("/characters", lotrController.getCharacters);
 
 module.exports = router;
